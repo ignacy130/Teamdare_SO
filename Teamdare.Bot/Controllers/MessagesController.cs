b618d7e6bdc875540812818e12fc63a6230d1c30
Pawel Sygnowski <pawelsygnowski@Pawels-MacBook-Pro.local>
2017-02-16 22:02:02 +0100

    Initial commit

---
+++ Teamdare.Bot/Controllers/MessagesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+
+namespace Teamdare.Bot.Controllers
+{
+    /// <summary>
+    /// This controller will receive the skype messages and handle them to the EchoBot service.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        /// <summary>
+        /// This method will be called every time the bot receives an activity. This is the messaging endpoint
+        /// </summary>
+        /// <param name="activity">The activity sent to the bot. I'm using dynamic here to simplify the code for the post</param>
+        /// <returns>201 Created</returns>
+        [HttpPost]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity != null)
+            {
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                var reply = activity.CreateReply();
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        reply.Text = activity.Text;
+                        await connector.Conversations.ReplyToActivityAsync(reply);
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                        if (activity.MembersAdded.Any())
+                        {
+                            reply.Text = "Welcome! ;-)";
+                            await connector.Conversations.ReplyToActivityAsync(reply);
+                        }
+                        break;
+                }
+            }
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+
+    }
+}
---
+++ Teamdare.Bot/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Teamdare.Bot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Teamdare.Bot")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("D38D3912-EAB0-4508-9EB5-87F9EF2B485F")]
