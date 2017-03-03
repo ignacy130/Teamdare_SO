5e6915c1531e686eee70361caf0565752f45b04d
Pawel Sygnowski <pawelsygnowski@Pawels-MacBook-Pro.local>
2017-03-03 21:54:09 +0100

    Fix for unauthorized exception in library https://github.com/Microsoft/BotBuilder/issues/2237

---
+++ Teamdare.Bot/Authentication/TokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Teamdare.Bot.Authentication
+{
+    public class TokenResponse
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+    }
+}
--- Teamdare.Bot/WebModule.cs
+++ Teamdare.Bot/WebModule.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using Autofac;
+using Microsoft.Extensions.Caching.Memory;
+using Teamdare.Bot.Authentication;
 using Teamdare.Bot.Communications;
 using Teamdare.Bot.Communications.Channels;
 using Module = Autofac.Module;
@@ -13,8 +15,10 @@ namespace Teamdare.Bot
             base.Load(builder);
 
             builder.RegisterType<Responses>().As<Responses>();
+            builder.RegisterType<MemoryCache>().As<IMemoryCache>().SingleInstance();
             builder.RegisterType<CommunicationChannel>().As<CommunicationChannel>();
             builder.RegisterType<CommunicationChannelMap>().As<CommunicationChannelMap>();
+            builder.RegisterType<BotCredentials>().As<BotCredentials>();
             builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
                 .Where(t => t.IsAssignableTo<IChannel>())
                 .AsSelf();
