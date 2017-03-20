5935f8203bfe23d31f500b0ff086fde41af1055f
Mateusz Chodkowski <chotkos@gmail.com>
2017-03-20 22:13:31 +0100

    worker fixes

---
+++ TeamDate.Worker/WorkerModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Teamdare.Worker.WorkerFunctions.Hourly;
+
+namespace Teamdare.Worker
+{
+    public class WorkerModule : Module
+    { 
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterType<TaskReminder>().As<TaskReminder>();
+            base.Load(builder); 
+        }
+    }
+}
